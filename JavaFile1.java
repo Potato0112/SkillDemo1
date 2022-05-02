b4ab62d8822b5cccc2b8f81dbecd055b9d79431d
Potato0112 <103155948+Potato0112@users.noreply.github.com>
2022-05-01 21:14:36 -0700

    commit1

--- JavaFile1.java
+++ JavaFile1.java
@@ -1,3 +1,5 @@
 public class JavaFile1 {
-
+    public static int multiplyby2(int i){
+        return 3*i;
+    }
 }
--- JavaFile1Tester.java
+++ JavaFile1Tester.java
@@ -1,3 +1,11 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class JavaFile1Tester {
-  
+    @Test  
+    public void multiplyby2test(){
+        assertEquals(6, JavaFile1.multiplyby2(3));
+    }
 }
